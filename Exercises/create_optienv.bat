b99b0facf204fe48db6e518575ac3db25d55ca1a
Richard Oberdieck <RICOB@orsted.dk>
2019-05-26 10:34:29 +0200

    First complete commit

---
+++ Exercises/create_optienv.bat
@@ -0,0 +1,3 @@
+call conda activate base
+conda env create -f opti_env.yml
+python -m ipykernel install --user --name optienv --display-name "Python (optienv)"
---
+++ Exercises/launch_jupyterlab.bat
@@ -0,0 +1,2 @@
+call conda activate base
+start jupyter lab 
